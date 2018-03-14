c1b7508dd41c86f8d2030b8953313c3607055664
Corentin LANGLAIT <clanglai@e3r2p23.42.fr>
2018-03-14 13:54:18 +0100

    Retry

--- get_next_line.c
+++ get_next_line.c
@@ -6,84 +6,73 @@
 /*   By: clanglai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 11:36:30 by clanglai          #+#    #+#             */
-/*   Updated: 2017/11/29 10:42:46 by clanglai         ###   ########.fr       */
+/*   Updated: 2018/03/14 13:47:16 by clanglai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static	int	ft_concat_to_line(char **tmp, char **line, char **buff)
+int			find_next_line(char *str)
 {
-	if (!(*tmp = ft_strjoin(*line, *buff)))
-		return (0);
-	free(*line);
-	if (!(*line = ft_strdup(*tmp)))
-		return (0);
-	free(*tmp);
-	ft_strclr(*buff);
-	return (1);
-}
-
-static	int	ft_readfile(const int fd, char **line, char **save, int state)
-{
-	int		ret;
-	char	*tmp;
-	char	*buff;
-
-	buff = ft_strnew(BUFF_SIZE);
-	while (state == 1)
-		if ((ret = read(fd, buff, BUFF_SIZE)) > 0)
-		{
-			buff[ret] = '\0';
-			if (ft_strchr(buff, '\n'))
-			{
-				free(*save);
-				*save = ft_strdup(ft_strchr(buff, '\n') + 1);
-				state = 2;
-				buff[ret - ft_strlen(ft_strchr(buff, '\n'))] = '\0';
-			}
-			if (!(ft_concat_to_line(&tmp, line, &buff)))
-				return (-1);
-		}
-		else if (ret == 0)
-			return (0);
-		else
-			return (-1);
-	free(buff);
-	return (state);
-}
-
-int			get_line(const int fd, char **line, int ret)
-{
-	static char	*save;
-
-	if (!save)
-		save = ft_strnew(BUFF_SIZE);
-	*line = ft_strnew(BUFF_SIZE);
-	if (ft_strchr(save, '\n') != NULL)
+	int i;
+	
+	i = 0;
+	if (!str[i])
+		return (-1);
+	while (str[i])
 	{
-		*line = ft_strncat(*line, save, ft_strlen(save) -
-				ft_strlen(ft_strchr(save, '\n')));
-		save = ft_strdup(ft_strchr(save, '\n') + 1);
-		ret = 1;
+		if (str[i] == '\n')
+			return (i);
+		i++;
 	}
-	else
-	{
-		*line = ft_strcat(*line, save);
-		free(save);
-		save = ft_strnew(BUFF_SIZE);
-	}
-	if (ft_strlen(save) == 0 && ret != 1)
-		if ((ret = ft_readfile(fd, line, &save, 1)) == -1)
-			return (-1);
-	if ((ft_strlen(*line)) == 0 && ret == 0)
-		return (0);
-	return (1);
+	return (-1);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	if (fd < 0 || !line)
+	static char *save;
+	int			ret;
+
+	if (!save)
+		save = ft_strnew(BUFF_SIZE);
+	if (fd == -1 || line == NULL)
 		return (-1);
-	return (get_line(fd, line, 0));
+	*line = ft_strdup(save);
+	ret = 0;
+	if (ft_strchr(save, '\n'))
+		save = ft_strchr(save, '\n') + 1;
+	else
+	{
+		ft_strclr(save);
+		while ((ret = read(fd, save, BUFF_SIZE)) > 0)
+		{
+			save[ret] = '\0';
+			*line = ft_strjoin(*line, save);
+			if (find_next_line(*line) >= 0)
+			{
+				save = ft_strchr(save, '\n') + 1;
+				break;
+			}
+		}
+	}
+	if (find_next_line(*line) != -1)
+		(*line)[find_next_line(*line)] = '\0';
+	if (ret < 0)
+		return (-1);
+	return (ret == 0 && ft_strlen(*line) == 0 ? 0 : 1);
+}
+
+int main(int argc, char **argv)
+{
+	char *line;
+	int fd;
+
+	fd = open(argv[argc - 1], O_RDONLY);
+	while(get_next_line(fd, &line) == 1)
+	{
+	printf("%s\n", line);
+		free(line);
+	}
+	free(line);
+	return (0);
 }
